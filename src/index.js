cefa707f25b242c39f2ec343e1b3c9ad88a7dc98
Muhammad Usman <61974138+usmandevops123@users.noreply.github.com>
2021-02-07 11:50:48 -0800

    upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"; 
+import ReactDom from "react-dom"; 
+import "./index.css"; 
+import Calc from "./Calc"
+//import {add,sub,mult,divd} from "./Heading";
+//import youtube from "./Heading";
+
+
+ReactDom.render(<Calc></Calc>,document.getElementById("root"));
